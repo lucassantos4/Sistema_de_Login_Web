b1f5760aeafa55105769a1350cac4f52eec35114
Lucas Rocha Santos <lucas.santos04@souunit.com.br>
2026-03-14 17:47:45 -0300

    implementação sistema login

--- src/main/java/br/appLogin/appLogin/controller/LoginController.java
+++ src/main/java/br/appLogin/appLogin/controller/LoginController.java
@@ -1,31 +1,64 @@
 package br.appLogin.appLogin.controller;
 import br.appLogin.appLogin.model.Usuario;
+import br.appLogin.appLogin.repository.UsuarioRepository;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.naming.Binding;
+import java.util.concurrent.LinkedBlockingDeque;
+
 @Controller
 public class LoginController {
+    @Autowired
+    private UsuarioRepository ur;
 
     @GetMapping("/login")
     public String login() {
         return "login";
     }
 
+    @GetMapping("/")
+    public String dashboard() {
+        return "index";
+    }
+
+    @PostMapping("/login")
+    public String loginUsuario(Usuario usuario, Model model, HttpServletResponse response ) {
+        Usuario usuarioLogado = this.ur.login(usuario.getEmail(), usuario.getSenha());
+        if(usuarioLogado != null) {
+            return "redirect:/";
+
+        }
+        model.addAttribute("erro","Usuario Invalido!");
+        return "login";
+
+    }
+
+
     @GetMapping("/cadastroUsuario")
     public String cadastro() {
         return "cadastro";
     }
 
-//    @RequestMapping(value = "cadastroUsuario", method = RequestMethod.POST) {
-//        public String cadastroUsuario() {
-//
-//          EM PROD PAPAPITOW.
-//
-//            return "";
-//        }
-//    }
+    @RequestMapping(value = "/cadastroUsuario", method = RequestMethod.POST)
+    public String cadastroUsuario(@Valid Usuario usuario, BindingResult result) {
 
+        if (result.hasErrors()) {
+            return "redirect:/cadastroUsuario";
+        }
+        ur.save(usuario);
+        return "redirect:/login";
+    }
 
 }
+
+
--- src/main/java/br/appLogin/appLogin/model/Usuario.java
+++ src/main/java/br/appLogin/appLogin/model/Usuario.java
@@ -24,4 +24,33 @@ public class Usuario {
     @NotEmpty
     private  String senha;
 
+    public long getId() {
+        return id;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
 }
